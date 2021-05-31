2c185a48c4651c2c8f41cf43728e132a0adca2eb
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2021-05-31 12:55:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import SearchBar from "/src/components/SearchBar";
+import VideoList from "/src/components/VideoList";
+import VideoDetail from "/src/components/VideoDetail";
+import useVideos from "/src/hooks/useVideo";
+
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("buildings");
+
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
